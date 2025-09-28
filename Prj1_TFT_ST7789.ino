6a1d9c018e8ae70039b5a9a244abe53025aad91a
Tờ <tathung140@gmail.com>
2025-09-28 11:47:24 +0700

    Updated full code

--- Prj1_TFT_ST7789.ino
+++ Prj1_TFT_ST7789.ino
@@ -11,6 +11,7 @@
 // Include các thư viện cần thiết
 #include <TFT_eSPI.h>
 #include "logoelab.h"
+#include "sontungmtp.h"
 
 TFT_eSPI tft = TFT_eSPI(); //Khởi tạo đối tượng tft
 
@@ -20,11 +21,40 @@ void setup() {
   tft.setSwapBytes(true); //Đảo màu nếu lỗi màu
   tft.fillScreen(TFT_BLACK); //Fill màn hình với màu đen
 
-  //Đẩy hình ảnh lên trên màn hình
-  tft.pushImage(0, 0, 240, 240, logoelab);
-
+  Serial.begin(115200);
+  Serial.println("Connected");
 }
 
 void loop() {
-  
+  unsigned long time = millis();
+
+  //Loading screen
+  while(time <= 10000) {
+    tft.drawString("LOADING .", 60, 120, 4);
+    delay(1000);
+    tft.drawString("LOADING ..", 60, 120, 4);
+    delay(1000);
+    tft.drawString("LOADING ...", 60, 120, 4);
+    delay(1000);
+    tft.drawString("LOADING ....", 60, 120, 4);
+    delay(1000);
+    tft.fillScreen(TFT_BLACK);
+    time = millis();
+  }
+
+
+  //Start
+  Serial.println("Flop quá thì ghi tên anh vào");
+  tft.pushImage(0, 0, 240, 240, logoelab);
+  delay(5000);
+  tft.fillScreen(TFT_BLACK);
+  tft.drawString("Flop Qua", 70, 90, 4);
+  delay(2000);
+  tft.drawString("Thi Ghi Ten Anh Vao", 5, 120, 4);
+  delay(2000);
+  tft.drawString("-Ba tuoc Saint Tone Emtippy-", 60, 180, 1);
+  delay(5000);
+  tft.pushImage(0, 0, 240, 240, sontungmtp);
+  delay(10000);
+  tft.fillScreen(TFT_BLACK);
 }
